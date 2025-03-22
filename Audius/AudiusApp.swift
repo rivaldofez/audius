37270dc86b17a94d6f69205f986ca1bc3adebdab
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-03-22 16:30:57 +0700

    [FEAT] Setup base project

---
+++ Audius/AudiusApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudiusApp.swift
+//  Audius
+//
+//  Created by Rivaldo Fernandes on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AudiusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
